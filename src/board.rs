324419123bd5d7e6bb0ed8ec61e93e2f8618aa18
Ricardo Sacramento <sacramentor@webeo.com>
2024-06-13 10:29:17 +0100

    feat: adding game logic

--- src/board.rs
+++ src/board.rs
@@ -1,10 +1,17 @@
-use std::fmt::Display;
+use std::{fmt::Display, str::FromStr};
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Play {
     Circle,
     Cross,
     Null,
+}
+
+enum Direction {
+    North,
+    East,
+    South,
+    West,
 }
 
 impl Play {
@@ -17,10 +24,32 @@ impl Play {
     }
 }
 
+#[derive(Debug)]
 pub struct Coordinate {
-    x: u8,
-    y: u8,
+    x: usize,
+    y: usize,
     play: Play,
+}
+
+impl FromStr for Coordinate {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts = s.chars().collect::<Vec<_>>();
+        println!("{:?}", parts);
+        if parts.len() != 3 {
+            return Err(anyhow::anyhow!("bad play!"));
+        }
+        return Ok(Coordinate {
+            x: parts[0].to_string().parse()?,
+            y: parts[1].to_string().parse()?,
+            play: match parts[2].to_string().to_lowercase().as_str() {
+                "x" => Play::Cross,
+                "o" => Play::Circle,
+                _ => Play::Null,
+            },
+        });
+    }
 }
 
 pub struct Game {
@@ -34,16 +63,51 @@ impl Game {
             [Play::Null, Play::Null, Play::Null],
             [Play::Null, Play::Null, Play::Null],
         ];
-        Game{board}
+        Game { board }
     }
 
     pub fn print_board(&self) {
-        self.board
-            .iter()
-            .for_each(|f| {
-                f.iter().for_each(|j| print!(" {} ", j.value()));
-                println!();
-            });
+        self.board.iter().for_each(|f| {
+            f.iter().for_each(|j| print!(" {} ", j.value()));
+            println!();
+        });
+    }
+    pub fn insert_coordinate(&mut self, c: Coordinate) {
+        self.board[c.y][c.x] = c.play;
+    }
+
+    pub fn is_win(&self, c: Coordinate, count: usize) -> usize {
+        todo!()
+    }
+
+    fn check_win_direction(
+        board: [[Play; 3]; 3],
+        mut x: usize,
+        mut y: usize,
+        play: Play,
+        mut win: usize,
+        direction: Direction,
+    ) -> bool {
+        let mut is_win = false;
+        if board[y][x] == play {
+            win = win + 1;
+            let is_win = if win == 3 {
+                true
+            } else { false };
+        } else {
+            match direction {
+                Direction::North => y = y - 1,
+                Direction::East => x = x + 1,
+                Direction::South => y = y + 1,
+                Direction::West => x = x - 1,
+            }
+            if x < 0 || x > 2 || y < 0 || y > 2 {
+                return false;
+            } else {
+                return Game::check_win_direction(board, x, y, play, win, direction);
+            }
+        }
+        is_win
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
+use std::io;
+
 use board::Game;
+
+use crate::board::Coordinate;
 
 mod board;
 
-fn main() {
+fn main() -> Result<(), anyhow::Error> {
     let game = Game::new();
     game.print_board();
+
+    let mut play = String::new();
+    println!("Insert a line and a column to play.");
+    io::stdin()
+        .read_line(&mut play)
+        .expect("Failed to read play");
+
+    println!("play string --> {}", play);
+    let play: Coordinate = play.trim().parse()?;
+
+    println!("{:?}", play);
+
+    Ok(())
 }
